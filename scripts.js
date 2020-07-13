2f0a03eb20a52e1fc9612a7276d16163fabf4069
unknown <zikkops@gmail.com>
2020-07-14 02:46:19 +0300

    fade-in / slide-in

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const faders = document.querySelectorAll('.fade-in')
+const sliders = document.querySelectorAll('.slide-in')
+
+
+const appearOptions={
+    rootMargin:"-250px",
+    threshold:0
+};
+
+
+const appearOnScroll =new IntersectionObserver(function(entries,appearOnScroll){
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting)
+        {return}
+        else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+
+},appearOptions)
+
+faders.forEach(fader=>{
+    appearOnScroll.observe(fader)
+})
+
+sliders.forEach(slider=>{
+    appearOnScroll.observe(slider);
+})
